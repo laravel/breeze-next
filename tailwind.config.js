5c2466b1197a07c29897f4186817bcc4e2eccec3
Gareth Redfern <gareth@garethredfern.com>
2022-02-11 21:21:15 +0000

    update tailwind to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    purge: ['./src/**/*.js'],
+    content: ['./src/**/*.js'],
     darkMode: 'media',
     theme: {
         extend: {
