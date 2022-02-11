c5488fe1ddf46b95aba4f81bee1b90a1c9c0f081
Gareth Redfern <gareth@garethredfern.com>
2022-02-11 21:46:09 +0000

    add type-safe equality operator

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -11,7 +11,7 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
             .get('/api/user')
             .then(res => res.data)
             .catch(error => {
-                if (error.response.status != 409) throw error
+                if (error.response.status !== 409) throw error
 
                 router.push('/verify-email')
             }),
