9038c0b0d543569eae8e219a8ff462d583590ea7
AquaWolfDev <aquawolfdev@protonmail.com>
2022-07-17 20:51:16 -0500

    added an example for preventing authenticated routes from flashing if user is not login

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,30 +1,51 @@
 import AppLayout from '@/components/Layouts/AppLayout'
 import Head from 'next/head'
+import { useAuth } from '@/hooks/auth'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const Dashboard = () => {
-    return (
-        <AppLayout
-            header={
-                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                    Dashboard
-                </h2>
-            }>
+  /*
+    This is an example of how to protect a route from unauthenticated user
+    
+    === Logic ===
+    first get the user from useAuth()
+    check if user exist
+    if false return null to prevent the page from flashing
+    if true show component
+  */
+  const router = useRouter()
+  const { user } = useAuth()
 
-            <Head>
-                <title>Laravel - Dashboard</title>
-            </Head>
+  useEffect(() => {
+    if (!user) router.push("/login")
+  }, [user])
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">
-                            You're logged in!
-                        </div>
-                    </div>
-                </div>
+  if (!user) return null
+
+  return (
+    <AppLayout
+      header={
+        <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+          Dashboard
+        </h2>
+      }>
+
+      <Head>
+        <title>Laravel - Dashboard</title>
+      </Head>
+
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div className="p-6 bg-white border-b border-gray-200">
+              You're logged in!
             </div>
-        </AppLayout>
-    )
+          </div>
+        </div>
+      </div>
+    </AppLayout>
+  )
 }
 
 export default Dashboard
