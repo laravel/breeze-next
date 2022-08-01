a61c3ed4ea7c88e34c68ff80615774b6f253f979
Dries Vints <dries@vints.io>
2022-08-01 16:55:11 +0200

    wip

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,7 +11,7 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"
+                        href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap"
                         rel="stylesheet"
                     />
                 </Head>
