d45a46638698932bbbebe4173cc9c05e6b1d1a0f
sentisso <sentisso.dev@gmail.com>
2022-09-02 18:22:40 +0200

    fixing tailwind not working with .tsx files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    content: ['./src/**/*.js'],
+    content: ['./src/**/*.tsx'],
     darkMode: 'media',
     theme: {
         extend: {
