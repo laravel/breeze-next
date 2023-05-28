ceaefe563d24d573bf9610aa323ace47f99a7c9e
Charlie Joseph <ch@rli.es>
2023-05-28 20:36:48 +0100

    Remove legacy src directory

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({ type = 'submit', className, ...props }) => (
-    <button
-        type={type}
-        className={`${className} inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150`}
-        {...props}
-    />
-)
-
-export default Button
