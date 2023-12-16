7cb69811fb880958d81f904a244873a175ddfec0
James Allen <jacques2186@yahoo.com>
2023-12-16 07:48:34 -0500

    Move header layout back to main layout

--- src/app/(app)/dashboard/layout.js
+++ src/app/(app)/dashboard/layout.js
@@ -1,7 +1,11 @@
 const AppLayout = ({ children, header }) => {
     return (
         <>
-            {header}
+            <header className="bg-white shadow">
+                <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {header}
+                </div>
+            </header>
 
             <main>{children}</main>
         </>
