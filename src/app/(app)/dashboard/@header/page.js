d3b6373eefd4988f5b186484e5e40a89c5b7a4a5
James Allen <jacques2186@yahoo.com>
2023-12-16 07:40:03 -0500

    Drop @header layout file

--- src/app/(app)/dashboard/@header/page.js
+++ src/app/(app)/dashboard/@header/page.js
@@ -1,8 +1,12 @@
 const Header = () => {
     return (
-        <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-            Dashboard
-        </h2>
+        <header className="bg-white shadow">
+            <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Dashboard
+                </h2>
+            </div>
+        </header>
     )
 }
 
