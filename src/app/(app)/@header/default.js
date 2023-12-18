3cf5b9fb0c8e64d521ef67e6009dfcc31a71404c
James Allen <jacques2186@yahoo.com>
2023-12-17 20:19:58 -0500

    Add @header back the correct way

---
+++ src/app/(app)/@header/default.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+            Dashboard
+        </h2>
+    )
+}
+
+export default Header
--- src/app/(app)/layout.js
+++ src/app/(app)/layout.js
@@ -3,14 +3,20 @@
 import { useAuth } from '@/hooks/auth'
 import Navigation from '@/app/(app)/Navigation'
 
-const AppLayout = ({ children }) => {
+const AppLayout = ({ children, header }) => {
     const { user } = useAuth({ middleware: 'auth' })
 
     return (
         <div className="min-h-screen bg-gray-100">
             <Navigation user={user} />
 
-            {children}
+            <header className="bg-white shadow">
+                <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {header}
+                </div>
+            </header>
+
+            <main>{children}</main>
         </div>
     )
 }
