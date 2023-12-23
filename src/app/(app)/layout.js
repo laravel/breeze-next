b3abffcd3b9a909b435555673ed07b7f18c3d507
James Allen <jacques2186@yahoo.com>
2023-12-23 07:22:38 -0500

    Add loading to avoid page flicker

--- src/app/(app)/layout.js
+++ src/app/(app)/layout.js
@@ -2,10 +2,13 @@
 
 import { useAuth } from '@/hooks/auth'
 import Navigation from '@/app/(app)/Navigation'
+import Loading from '@/app/(app)/Loading'
 
 const AppLayout = ({ children, header }) => {
     const { user } = useAuth({ middleware: 'auth' })
-
+    if (!user) {
+        return <Loading />
+    }
     return (
         <div className="min-h-screen bg-gray-100">
             <Navigation user={user} />
