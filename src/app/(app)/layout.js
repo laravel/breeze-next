3e172721c1d0e7d98b85173008cfede2112a7441
Taylor Otwell <taylor@laravel.com>
2023-12-23 09:37:08 -0600

    wip

--- src/app/(app)/layout.js
+++ src/app/(app)/layout.js
@@ -6,9 +6,11 @@ import Loading from '@/app/(app)/Loading'
 
 const AppLayout = ({ children, header }) => {
     const { user } = useAuth({ middleware: 'auth' })
+
     if (!user) {
         return <Loading />
     }
+
     return (
         <div className="min-h-screen bg-gray-100">
             <Navigation user={user} />
