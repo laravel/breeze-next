f1906dc2b62f49e7689c6ff8a4c1fdeb26f191ed
Devstack <natanvdb@hotmail.com>
2024-02-16 22:35:07 +0100

    Fix app router features
    - Fix unnecessary use of parallels routes for a "header" layout title and replace it with a simple component "Header"
    - Convert useRouter().pathname to usePathname according to documentation (useRouter().pathname was undefined and the active props of NavLInk weren't working).

--- src/app/(app)/dashboard/page.js
+++ src/app/(app)/dashboard/page.js
@@ -1,18 +1,23 @@
+import Header from '@/app/(app)/Header'
+
 export const metadata = {
     title: 'Laravel - Dashboard',
 }
 
 const Dashboard = () => {
     return (
-        <div className="py-12">
-            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div className="p-6 bg-white border-b border-gray-200">
-                        You're logged in!
+        <>
+            <Header title="Dashboard" />
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 bg-white border-b border-gray-200">
+                            You are logged in!
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
