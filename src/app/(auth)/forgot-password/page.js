e56e01c94db04ec908862018c411db2a30ae2f3b
Musanna Al Akil <43068505+Musanna-al-akil@users.noreply.github.com>
2024-05-21 03:10:02 +0600

    removed duplicate word from app/(auth)/forgot-password/page.js (#50)
    There was 2 duplicate words. (email and that).

--- src/app/(auth)/forgot-password/page.js
+++ src/app/(auth)/forgot-password/page.js
@@ -28,8 +28,8 @@ const Page = () => {
         <>
             <div className="mb-4 text-sm text-gray-600">
                 Forgot your password? No problem. Just let us know your email
-                email address and we will email you a password reset link that
-                that will allow you to choose a new one.
+                address and we will email you a password reset link that
+                will allow you to choose a new one.
             </div>
 
             {/* Session Status */}
