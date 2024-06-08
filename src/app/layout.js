404f1a2f8c6c2337b6100fbeaaf432a031fe15e5
Willem-Jaap <willemjaapk@gmail.com>
2024-06-08 20:11:49 +0200

    refactor: use `next/font` for more optimized font loading

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,21 @@
+import { Nunito } from 'next/font/google'
 import '@/app/global.css'
 
-export const metadata = {
-    title: 'Laravel',
-}
+const nunitoFont = Nunito({
+    subsets: ['latin'],
+    display: 'swap',
+})
+
 const RootLayout = ({ children }) => {
     return (
-        <html lang="en">
+        <html lang="en" className={nunitoFont.className}>
             <body className="antialiased">{children}</body>
         </html>
     )
+}
+
+export const metadata = {
+    title: 'Laravel',
 }
 
 export default RootLayout
